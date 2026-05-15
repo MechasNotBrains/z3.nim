be35163bc77a5a7c0eab89ae9af1c366abc7e729
sOkam <7308253+heysokam@users.noreply.github.com>
2026-05-15 15:06:00 +0200

    bld: Generator minimal setup

---
+++ gen/generator.nim
@@ -0,0 +1,19 @@
+from std/os import `/`, parentDir
+import henka
+
+const z3Api  {.strdefine.}= currentSourcePath().parentDir()/".."/"bin"/".lib"/"z3"/"src"/"api"
+const outDir {.strdefine.}= currentSourcePath().parentDir()/".."/"src"
+
+proc filter(kind: LabelKind, name: string): bool =
+  if kind == Constant: return false
+  result = true
+
+writeFile(outDir/"z3.nim", generate(
+  inputFile    = z3Api/"z3.h",
+  clangArgs    = @["-I" & z3Api],
+  linkMode     = LinkMode.dynlib,
+  dynlibName   = "z3Lib",
+  dynlibPath   = "libz3.so",
+  symbolFilter = filter,
+))
+
